4ed5270c040abe1c70b409d6e43d12099795cc6c
Yana-Denisova <ladymacbeth@list.ru>
2022-07-31 16:02:10 +0300

    test validate

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -122,7 +122,7 @@ class RecipePostSerializer(serializers.ModelSerializer):
         print(value)
         ingr_list = []
         for data in value:
-            ingr_id = data['id']
+            ingr_id = data['ingredients']
             if not data:
                 raise serializers.ValidationError('Укажите Ингредиенты')
             if ingr_id in ingr_list:
