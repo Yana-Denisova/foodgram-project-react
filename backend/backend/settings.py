ffcc3683f2fb7700eca6f4a06c7e120b251a49af
Yana-Denisova <ladymacbeth@list.ru>
2022-08-01 18:00:34 +0300

    permission

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -59,12 +59,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-#DATABASES = {
-    #'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #}
-#}
 DATABASES = {
 
     'default': {
